382d491536dff7e9061b49d75fdaf5acc4bb50bc
martinh118 <m.jaime@sapalomera.cat>
2021-12-29 18:48:05 +0100

    no me gusta git bash

---
+++ Exercici_4.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class Exercici_4 {
+        public static void main(final String[] args) {
+
+/*
+//EJERECICIO 1
+
+        Scanner scan = new Scanner(System.in);
+        int opcion;
+        
+
+        System.out.print("Elige una opción:" + "\n1 - Negro" + "\n2 - Rojo" + "\n3 - Blanco\n");
+        opcion = scan.nextInt();
+
+        if (opcion == 1){
+            System.out.println("Has elegido el color negro.");
+        }
+        else if (opcion == 2){
+            System.out.println("Has elegido el color rojo.");
+        }
+        else if (opcion == 3){
+            System.out.println("Has elegido el color blanco.");
+        }
+        scan.close();
+
+
+//EJERECICIO 2
+
+    Scanner scan = new Scanner(System.in);
+    int dni; //variable de numeros dni
+
+    System.out.print("Escribe los numeros de tu dni: "); //linea que muestra la pregunta a hacer.
+    dni = scan.nextInt(); //linea que escanea la respuesta del usuario y le da el valor a la variable dni.
+
+    final String LLETRES = "TRWAGMYFPDXBNJZSQVHLCKE"; //constante de letras con el orden de DNI.
+    int residu = dni % 23; //calcula el resto que queda diviendo la variable dni por 23.
+    char lletra = LLETRES.charAt(residu); //del resultado anterior en la variable residu... encuentra el resultado entre la constante de letras
+    System.out.println("Tu DNI completo es: " + dni + " " + lletra);
+
+
+
+//EJERECICIO 3
+    
+    Scanner scan = new Scanner(System.in);
+    int altura;
+    int base;
+
+    System.out.print("Escribe la altura de un triangulo rectangulo: ");
+    altura = scan.nextInt();
+
+    System.out.print("Escribe la base de un triangulo rectangulo: ");
+    base = scan.nextInt();
+
+    double hipotenusa = (altura * altura) + (base * base);
+    hipotenusa = Math.sqrt(hipotenusa);
+
+    double area = (altura * base) / 2;
+    double perimetro = altura + base + hipotenusa;
+
+    System.out.printf("Altura: %d \nBase: %d \nHipotenusa: %.3f \nPerimetro: %.3f \nArea: %.3f", altura, base, hipotenusa, perimetro, area);
+    scan.close();
+
+
+//EJERECICIO 4
+
+    Scanner scan = new Scanner(System.in);
+    int radio;
+
+
+    System.out.print("Elige el radio del circulo: ");
+    radio = scan.nextInt();
+
+    double area = (radio * radio) * Math.PI;
+    double perimetro = 2 * Math.PI * radio;
+
+    System.out.printf("Radio: %d \nArea: %,.6f  \nPerimetro: %,.6f", radio, area, perimetro);
+    scan.close();
+
+
+//EJERECICIO 5
+
+Scanner scan = new Scanner(System.in); //crea un objeto a escanear
+        byte edad; //variable de numero
+        
+        System.out.print("¿Que edad tienes? "); //pregunta la edad...
+        edad = scan.nextByte(); //y escanea la variable en numero 
+        
+        boolean conducir = edad >= 18 && edad <= 80;
+        boolean trabajar = edad >= 16 && edad <= 65;
+        boolean guarderia = edad < 3;
+        boolean CFGS = edad >= 18 && edad <= 24;
+
+        System.out.println("¿Puede conducir? " + conducir + "\n¿Puede trabajar? " + trabajar + "\n¿Debe ir a la guarderia? " + guarderia + "\n¿Puede estudiar en CFGS? " + CFGS);
+        scan.close();
+
+
+//EJERCICIO 6
+
+Scanner scan = new Scanner(System.in);
+        float nota_final;
+        int UF1;
+        int UF2;
+        int UF3;
+        int UF4;
+        int UF5;
+        int UF6;
+
+        System.out.print("Introduce las 6 notas de las UFs por espacios: ");
+        scan.nextFloat();    
+            scan.nextLine();
+*/
+        }
+}
